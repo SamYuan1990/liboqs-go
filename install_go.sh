70f4bb5a9593ee0a9b9681749bc090908541d960
Sam Yuan <yy19902439@126.com>
2023-12-09 21:08:41 +0800

    fix up
    Signed-off-by: Sam Yuan <yy19902439@126.com>

--- install_go.sh
+++ install_go.sh
@@ -3,6 +3,9 @@ get_arch=`uname -m`
 if [[ $get_arch =~ "x86_64" ]];then
     wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz
     rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz
+elif [[ $get_arch =~ "arrch64" ]];then
+    wget https://go.dev/dl/go1.21.5.linux-arm64.tar.gz
+    rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.5.linux-arm64.tar.gz
 else
     wget https://go.dev/dl/go1.21.5.linux-$(uname -m).tar.gz
     rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.5.linux-$(uname -m).tar.gz
